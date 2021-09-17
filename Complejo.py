9695a86b7de9e9d6565e770342eee27f79309ef4
AngelEscudero4 <50014613+AngelEscudero4@users.noreply.github.com>
2021-09-17 18:45:47 +0200

    Delete duplicate elements in getCaras and fix bug in getCarasDIm

--- Complejo.py
+++ Complejo.py
@@ -1,45 +1,51 @@
 from itertools import combinations
 
+
 class Complejo:
-    #recibe lista de los simplices maximales
+    # recibe lista de los simplices maximales
     def __init__(self, maximal_simplice_list):
         self.simplices = maximal_simplice_list
 
     def __print__(self):
         print(self.simplices)
-        
+
     def dim(self):
-        #la dimension del complejo es su simplice (lista) mas larga - 1
-        return len(max(self.simplices, key= len))-1
+        # la dimension del complejo es su simplice (lista) mas larga - 1
+        return len(max(self.simplices, key=len)) - 1
 
     def getCaras(self):
         aux = []
-        #recorremos los simplices maximales para construir las caras
+        # recorremos los simplices maximales para construir las caras
         for cada_simplice in self.simplices:
-            #para cada simplice construimos todas las caras (dimension menor que la del simplice)
+            # para cada simplice construimos todas las caras (dimension menor que la del simplice)
             for i in range(1, len(cada_simplice)):
-                aux = aux+list(combinations(cada_simplice, i))
-            #como combinations nos devuelve tuplas -> casteamos a listas
+                aux = aux + list(combinations(cada_simplice, i))
+        # como combinations nos devuelve tuplas -> casteamos a listas
+        # tambien tenemos que eliminar los repetidos ya que [0,1] y [0,2] generan dos veces el 0
+        aux = list(set(aux))
         caras = [list(x) for x in aux]
+        caras.sort(key=len)  # ordenar por tamaño de simplices
         return caras
 
     """
     Falta por probar.
     """
+
     def getCarasDim(self, dim):
         aux = []
-        listaSimplices=[]
-        #consigo los simplices que tengan una dimension superior o igual a la que quiero sacar las caras
+        listaSimplices = []
+        # consigo los simplices que tengan una dimension superior o igual a la que quiero sacar las caras
         for x in self.simplices:
-            if len(x) >= dim +1:   
-                listaSimplices=listaSimplices+ x
+            if len(x) >= dim + 1:
+                listaSimplices.append(x)
 
-        #recorremos los simplices maximales para construir las caras
+        # recorremos los simplices maximales para construir las caras
         for cada_simplice in listaSimplices:
-            #para cada simplice construimos todas las caras (dimension menor que la del simplice)
-            aux = aux+list(combinations(cada_simplice, dim))
-        #como combinations nos devuelve tuplas -> casteamos a listas
+            lista=list(combinations(cada_simplice, dim))
+            # para cada simplice construimos todas las caras (dimension menor que la del simplice)
+            aux = aux + lista
+        # como combinations nos devuelve tuplas -> casteamos a listas
         caras = [list(x) for x in aux]
         return caras
 
-    #recomendable hacer funcion auxiliar que nos diga si un elem es cara de otro elem
+    # recomendable hacer funcion auxiliar que nos diga si un elem es cara de otro elem
--- main.py
+++ main.py
@@ -8,3 +8,5 @@ ejemplo_1 = Complejo([[0,1,2],[1,3,4],[0,7,9,8],[6,5]])
 
 print('Dimension: ', ejemplo_1.dim())
 print('Caras: ', ejemplo_1.getCaras())
+for i in range(3):
+    print('Caras dim'+str(i+1)+': ', ejemplo_1.getCarasDim(i+1))
