c6e3a691a16e435182964a6887bb38781e3ad04b
Alex-Tibu <73992378+Alex-Tibu@users.noreply.github.com>
2021-09-17 16:45:04 +0200

    getCaras añadido

--- Complejo.py
+++ Complejo.py
@@ -4,8 +4,23 @@ class Complejo:
     #recibe lista de los simplices maximales
     def __init__(self, maximal_simplice_list):
         self.simplices = maximal_simplice_list
+
+    def __print__(self):
+        print(self.simplices)
         
     def dim(self):
         #la dimension del complejo es su simplice (lista) mas larga - 1
         return len(max(self.simplices, key= len))-1
+
+    def getCaras(self):
+        aux = []
+        #recorremos los simplices maximales para construir las caras
+        for cada_simplice in self.simplices:
+            #para cada simplice construimos todas las caras (dimension menor que la del simplice)
+            for i in range(1, len(cada_simplice)):
+                aux = aux+list(combinations(cada_simplice, i))
+            #como combinations nos devuelve tuplas -> casteamos a listas
+        caras = [list(x) for x in aux]
+        return caras
+
 
---
+++ main.py
@@ -0,0 +1,10 @@
+from Complejo import Complejo
+from itertools import combinations
+
+from Complejo import Complejo
+
+
+ejemplo_1 = Complejo([[0,1,2],[1,3,4],[0,7,9,8],[6,5]])
+
+print('Dimension: ', ejemplo_1.dim())
+print('Caras: ', ejemplo_1.getCaras())
