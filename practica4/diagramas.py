68a015d0965a740ac300e75a23000a3f5f9ed5b8
AngelEscudero4 <angelico4@hotmail.es>
2021-12-03 16:29:02 +0100

    clean code

--- practica4/diagramas.py
+++ practica4/diagramas.py
@@ -1,7 +1,18 @@
+from matplotlib.pyplot import annotate
+
 from practica1 import Complejo
 import numpy as np
 from practica3 import homologia
 from matplotlib import pyplot as plt
+
+
+def simplices_ordenados(complejo: Complejo):
+    # primero sacamos todos los simplices posibles
+    simplices = complejo.getCaras()
+    simplices = list(simplices)
+    # ahora vamos a tener que ordenarlos por longitud
+    simplices.sort(key=len)
+    return simplices
 
 
 def matriz_borde_generalizada(complejo: Complejo):
@@ -9,16 +20,12 @@ def matriz_borde_generalizada(complejo: Complejo):
     Dado un complejo simplicial calculamos su matriz borde generalizada
     *** MATRIZ [FILAS][COL] ***
     """
-    # primero sacamos todos los simplices posibles
-    simplices = complejo.getCaras()
-    simplices = list(simplices)
-    # ahora vamos a tener que ordenarlos por longitud (habria que mirar por peso tmbn)
-    simplices.sort(key=len)
+    simplices = simplices_ordenados(complejo)
     print(simplices)
     # ahora tenemos que hacer la matriz de incidencia
     matriz = np.zeros((len(simplices), len(simplices)))
     matriz = matriz.tolist()
-    # hay que poner un 1 en la casilla [i][j] si el simplice_dim_p1[i] es cara de simplice_dim_p[j]
+    # hay que poner un 1 en la casilla [i][j] si el simplice_dim_(p-1)[i] es cara de simplice_dim_p[j]
     for i in range(homologia.num_filas(matriz)):
         for j in range(homologia.num_columnas(matriz)):
             if len(simplices[i]) == len(simplices[j]) - 1 and Complejo.esCara(simplices[i], simplices[j]):
@@ -28,32 +35,33 @@ def matriz_borde_generalizada(complejo: Complejo):
 
 def algoritmo_emparejamiento_nacimiento_muerte(matriz):
     """
-    Recibida una matriz vamos a calculas los emparejamientos nacimiento-muerte
+    Recibida una matriz borde gen vamos a calculas los emparejamientos nacimiento-muerte
     """
-    # dada una columna sacamos su low()
-    # miramos en las columnas de su izqda y si tienen el mismo low borramos ese low en la de la izqda
+    # dada una columna sacamos su low() miramos en las columnas de su izqda y si tienen el mismo low borramos ese low
+    # en la de la dcha, repetimos proceso hasta que no se repita posicion low con ninguno de su izqda
     for i in range(homologia.num_columnas(matriz)):
-        repetir = True  # V pq queremos que lo ejeute al menos una vez (equiv a do:while)
+        repetir = True  # True pq queremos que lo ejeute al menos una vez (equiv a do:while)
         while repetir:
             repetir = False  # Si no modificamos la columna es pq no hay un low igual
             index_low = get_low(get_columna(matriz, i))
             # si existe low en esa columna
             if index_low != -1:
-                # borramos los low iguales de columnas anteriores
+                # buscamos lows anteriores en el mismo sitio
                 for otro_i in range(i):
                     # si tienen el mismo low
                     if get_low(get_columna(matriz, otro_i)) == index_low:
-                        # sumamos las dos columnas en la que estamos actualmente
+                        # sumamos las dos columnas, en la que estamos actualmente ponemos el resultado
                         homologia.sumar_dos_columnas(matriz, otro_i, i)
-                        # si encontramos otro low ahora este ha sido modificado y debriamos mirar si este nuevo se vuelve a tener
+                        # si encontramos otro low ahora este ha sido modificado y debriamos mirar si este nuevo se
+                        # vuelve a repetir
                         repetir = True
     return matriz
 
 
 def get_low(col):
     """
-    Recibimos una columna y calculamos su 1 mas abajo.
-    Devolvemos su indice.
+    Recibimos una columna y calculamos el indice de su 1 mas abajo.
+    Devuelve -1 en caso de ser all 0's
     """
     indice = -1
     for i in range(len(col)):
@@ -72,17 +80,14 @@ def get_emparejamientos(matriz):
     Despues calculamos los emparejamientos para cada columna
 
     Emparejamientos son [low(j),j]
-    ##### OJO luego hay que sacar los simplices de la lista 'simplices' de matriz_borde_generalizada()
-
-    Para pasarlo luego a puntos en el diagrama hay que coger los pesos de los simplices
     """
     emparejamientos = []
+    # reduzco para que no haya lows repetidos
     algoritmo_emparejamiento_nacimiento_muerte(matriz)
 
     # recorremos las columnas
     for j in range(homologia.num_columnas(matriz)):
         # sacamos su low y creamos una parejita
-        # if index_low !=-1: # revisar si hace falta o no
         index_low = get_low(get_columna(matriz, j))
         # metemos la pareja
         emparejamientos.append((index_low, j))
@@ -94,7 +99,25 @@ def get_emparejamientos(matriz):
 # DISTINTOS OBJETOS
 
 
-def conseguir_puntos_diagrama(complejo, emparejamientos, simplices):
+def conseguir_puntos_diagrama(complejo):
+    matriz_borde_gen = matriz_borde_generalizada(complejo)
+
+    # si queremos ver la matriz podemos con esto
+    # imprimir_matriz(matriz_borde_gen)
+
+    # operamos para que no haya lows repetidos
+    matriz_borde_gen = algoritmo_emparejamiento_nacimiento_muerte(matriz_borde_gen)
+    # imprimir_matriz(matriz_borde_gen)
+
+    # cogemos los emparejamientos por pares
+    emparejamientos = get_emparejamientos(matriz_borde_gen)
+    # print("[low-columna]", emparejamientos)
+
+    # cogemos los simplices ordenados --> OJO QUE TIENEN QUE COINCIDIR CON LOS DE maatriz_borde_gen
+    simplices = simplices_ordenados(complejo)
+    print(simplices)
+
+    # ordenamos los simplices por pesos para poder sacar el asociado al simplice
     complejo.filtrationOrder()
     print(complejo.simplices)
     print(complejo.pesos)
@@ -102,6 +125,7 @@ def conseguir_puntos_diagrama(complejo, emparejamientos, simplices):
     lista_aristas = []
     for (low, columna) in emparejamientos:  # simplices[low] es nacimiento y simplices[col] es muerte
         if low != -1:
+            # si es un de H0 lo añado a los puntos, si H1 lo añado a aristas
             if complejo.devolverPeso(simplices[low]) == 0:
                 # low sera la fila y col columna --> sacamos simplices para sus pesos y pintar el punto
                 (x, y) = (complejo.devolverPeso(simplices[low]), complejo.devolverPeso(simplices[columna]))
@@ -110,19 +134,14 @@ def conseguir_puntos_diagrama(complejo, emparejamientos, simplices):
                 (x, y) = (complejo.devolverPeso(simplices[low]), complejo.devolverPeso(simplices[columna]))
                 lista_aristas.append([x, y])
 
-    lista_puntos.append(
-        (0, complejo.pesos[-1] + 1))  # añadimos el punto del infinito que sabemos que siempre va a estar ahi
-    # peso de la comp conexa del 0
+    # añadimos el punto del infinito que sabemos que siempre va a estar ahi peso de la comp conexa del 0
+    lista_puntos.append((0, complejo.pesos[-1] + 1))
 
-    # ahora pintamos los puntos
-    # plt.plot(lista_puntos)
-    # plt.show()
-    print(lista_puntos)
-    print(lista_aristas)
     return lista_puntos, lista_aristas
 
 
-def diagrama_barras(puntos_diagrama, arista_diagrama):
+def diagrama_barras(complejo):
+    puntos_diagrama, aristas_diagrama = conseguir_puntos_diagrama(complejo)
     peso_max = puntos_diagrama[-1][1]
     altura = 0.5
     for [x, y] in puntos_diagrama:
@@ -131,11 +150,11 @@ def diagrama_barras(puntos_diagrama, arista_diagrama):
         altura += 0.5
 
     altura += 0.25
-    intervalo = np.linspace(-0.5, peso_max+0.5)
+    intervalo = np.linspace(-0.5, peso_max + 0.5)
     plt.plot(intervalo, [altura] * len(intervalo), 'k')
     altura += 0.75
 
-    for [x, y] in arista_diagrama:
+    for [x, y] in aristas_diagrama:
         intervalo = np.linspace(x, y)
         plt.plot(intervalo, [altura] * len(intervalo), 'r')
         altura += 0.5
@@ -144,7 +163,9 @@ def diagrama_barras(puntos_diagrama, arista_diagrama):
     plt.show()
 
 
-def diagrama_persistencia(puntos_diagrama, arista_diagrama):
+def diagrama_persistencia(complejo):
+    puntos_diagrama, aristas_diagrama = conseguir_puntos_diagrama(complejo)
+
     peso_max = puntos_diagrama[-1][1]
 
     # si tenemos puntos los pintamos (H0)
@@ -152,9 +173,9 @@ def diagrama_persistencia(puntos_diagrama, arista_diagrama):
         puntos_diagrama = np.array(puntos_diagrama)
         plt.scatter(puntos_diagrama[:, 0], puntos_diagrama[:, 1], color="blue")
     # H1
-    if len(arista_diagrama) > 0:
-        arista_diagrama = np.array(arista_diagrama)
-        plt.scatter(arista_diagrama[:, 0], arista_diagrama[:, 1], color="red")
+    if len(aristas_diagrama) > 0:
+        aristas_diagrama = np.array(aristas_diagrama)
+        plt.scatter(aristas_diagrama[:, 0], aristas_diagrama[:, 1], color="red")
 
     # mostramos los ejes discontinuos
     intervalo = np.linspace(0, peso_max)
--- practica4/test_diagramas.py
+++ practica4/test_diagramas.py
@@ -21,24 +21,6 @@ complejo.anadirSimplice([(1,2,3)], 3)
 # complejo.anadirSimplice([(5,6,7)], 3)
 
 
-matriz_borde_gen = diagramas.matriz_borde_generalizada(complejo)
 
-imprimir_matriz(matriz_borde_gen)
-
-matriz_borde_gen = diagramas.algoritmo_emparejamiento_nacimiento_muerte(matriz_borde_gen)
-print("AQUI VAMOS A VER LA MATRIZ")
-imprimir_matriz(matriz_borde_gen)
-
-emparejamientos = diagramas.get_emparejamientos(matriz_borde_gen)
-print("low-columna", emparejamientos)
-
-# cogemos los simplices ordenados --> OJO QUE TIENEN QUE COINCIDIR CON LOS DE maatriz_borde_gen
-simplices = complejo.getCaras()
-simplices = list(simplices)
-# ahora vamos a tener que ordenarlos por longitud (habria que mirar por peso tmbn)
-simplices.sort(key=len)
-
-puntos_diagrama, aristas_diagrama = diagramas.conseguir_puntos_diagrama(complejo, emparejamientos, simplices)
-
-diagramas.diagrama_persistencia(puntos_diagrama, aristas_diagrama)
-diagramas.diagrama_barras(puntos_diagrama, aristas_diagrama)
+diagramas.diagrama_persistencia(complejo)
+diagramas.diagrama_barras(complejo)
